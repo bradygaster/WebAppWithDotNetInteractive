7c8f4aad29db2cc572b42cf87e6af3f61afa81ff
Brady Gaster <bradyg@microsoft.com>
2020-04-27 01:52:48 -0700

    feeding myself messages

--- DotNetInteractiveWorkerHost.cs
+++ DotNetInteractiveWorkerHost.cs
@@ -51,12 +51,14 @@ namespace WebAppHostingDotNetInteractive {
         {
             while (!stoppingToken.IsCancellationRequested)
             {
-                await Task.Delay(10000);
-                logger.LogDebug("Worker with host is running, sending sample to myself.");
+                logger.LogDebug($"Worker with host is running. DotNetInteractiveProcess.HasExited?: {DotNetInteractiveProcess.HasExited} sending sample to myself.");
 
+                // {"token":"1","commandType":"SubmitCode","command":{"code": "var a = 1 + 13;"}}
                 await DotNetInteractiveProcess.StandardInput.WriteLineAsync(
                     "{\"token\":\"1\",\"commandType\":\"SubmitCode\",\"command\":{\"code\": \"var a = 1 + 13;\"}}"
                 );
+
+                await Task.Delay(3000);
             }
         }
     }
