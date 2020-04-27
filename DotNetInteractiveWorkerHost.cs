904a81d52f4d79b01984c151ebd060fb1fa5a4ea
Brady Gaster <bradyg@microsoft.com>
2020-04-27 01:48:27 -0700

    trying this out

--- DotNetInteractiveWorkerHost.cs
+++ DotNetInteractiveWorkerHost.cs
@@ -5,7 +5,7 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
 namespace WebAppHostingDotNetInteractive {
-    public class DotNetInteractiveWorkerHost : IHostedService {
+    public class DotNetInteractiveWorkerHost : BackgroundService {
         private readonly ILogger<DotNetInteractiveWorkerHost> logger;
         public DotNetInteractiveWorkerHost (ILogger<DotNetInteractiveWorkerHost> logger) {
             this.logger = logger;
@@ -13,8 +13,10 @@ namespace WebAppHostingDotNetInteractive {
 
         public Process DotNetInteractiveProcess { get; private set; }
 
-        public Task StartAsync (CancellationToken cancellationToken) {
+        public override Task StartAsync (CancellationToken cancellationToken) {
             logger.LogInformation ("Starting up.");
+
+            base.StartAsync(cancellationToken);
 
             var startInfo = new ProcessStartInfo ("dotnet", "interactive stdio --default-kernel csharp") 
             {
@@ -25,19 +27,37 @@ namespace WebAppHostingDotNetInteractive {
             };
 
             DotNetInteractiveProcess = new Process {
+                EnableRaisingEvents = true,
                 StartInfo = startInfo
             };
 
+            DotNetInteractiveProcess.ErrorDataReceived += (s, e) => logger.LogError(e.Data);
+            DotNetInteractiveProcess.OutputDataReceived += (s, e) => logger.LogInformation(e.Data);
+            DotNetInteractiveProcess.Exited += (s, e) => logger.LogCritical(".NET Interactive Exited");
             DotNetInteractiveProcess.Start ();
 
             return Task.CompletedTask;
         }
 
-        public Task StopAsync (CancellationToken cancellationToken) {
+        public override Task StopAsync (CancellationToken cancellationToken) {
+            base.StopAsync(cancellationToken);
             logger.LogInformation("Stopping.");
             DotNetInteractiveProcess.Kill();
 
             return Task.CompletedTask;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                await Task.Delay(10000);
+                logger.LogDebug("Worker with host is running, sending sample to myself.");
+
+                await DotNetInteractiveProcess.StandardInput.WriteLineAsync(
+                    "{\"token\":\"1\",\"commandType\":\"SubmitCode\",\"command\":{\"code\": \"var a = 1 + 13;\"}}"
+                );
+            }
         }
     }
 }
